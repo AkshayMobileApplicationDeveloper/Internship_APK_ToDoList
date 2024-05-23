9363a34684d8036a205366ee86f7868c0f95a1c0
AkshayMobileApplicationDeveloper <akshaykumarprajapati@hotmail.com>
2024-05-23 22:31:51 +0530

    Database set

--- app/src/main/java/com/example/todolist/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/todolist/Adapter/ToDoAdapter.java
@@ -1,77 +1,107 @@
 package com.example.todolist.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todolist.MainActivity;
+import com.example.todolist.Model.ToDoModel;
+import com.example.todolist.R;
+import com.example.todolist.Utils.DataBaseHelper;
+import com.google.android.material.checkbox.MaterialCheckBox;
+
+import java.util.List;
+
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
-    /**
-     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
-     * an item.
-     * <p>
-     * This new ViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     * <p>
-     * The new ViewHolder will be used to display items of the adapter using
-     * {@link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
-     * different items in the data set, it is a good idea to cache references to sub views of
-     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
-     *
-     * @param parent   The ViewGroup into which the new View will be added after it is bound to
-     *                 an adapter position.
-     * @param viewType The view type of the new View.
-     * @return A new ViewHolder that holds a View of the given view type.
-     * @see #getItemViewType(int)
-     * @see #onBindViewHolder(ViewHolder, int)
-     */
+
+    private List<ToDoModel> modelList;
+    private MainActivity mainActivity;
+    private DataBaseHelper dataBaseHelper;
+
+    public ToDoAdapter(List<ToDoModel> modelList, MainActivity mainActivity, DataBaseHelper dataBaseHelper) {
+        this.modelList = modelList;
+        this.mainActivity = mainActivity;
+        this.dataBaseHelper = dataBaseHelper;
+    }
+
     @NonNull
     @Override
     public ToDoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ToDoAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.task_lauout,parent,false));
     }
 
-    /**
-     * Called by RecyclerView to display the data at the specified position. This method should
-     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
-     * position.
-     * <p>
-     * Note that unlike {@link ListView}, RecyclerView will not call this method
-     * again if the position of the item changes in the data set unless the item itself is
-     * invalidated or the new position cannot be determined. For this reason, you should only
-     * use the <code>position</code> parameter while acquiring the related data item inside
-     * this method and should not keep a copy of it. If you need the position of an item later
-     * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
-     * have the updated adapter position.
-     * <p>
-     * Override {@link #onBindViewHolder(ViewHolder, int, List)} instead if Adapter can
-     * handle efficient partial bind.
-     *
-     * @param holder   The ViewHolder which should be updated to represent the contents of the
-     *                 item at the given position in the data set.
-     * @param position The position of the item within the adapter's data set.
-     */
+
+
     @Override
     public void onBindViewHolder(@NonNull ToDoAdapter.ViewHolder holder, int position) {
+        final ToDoModel item= modelList.get(position);
+        holder.idMaterialCheckBoxitemView.setText(item.getTask());
+        boolean toBoolean;
+        holder.idMaterialCheckBoxitemView.setChecked(toBoolean(item.getStatus()) );
+        holder.idMaterialCheckBoxitemView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    dataBaseHelper.updateStatus(item.getStatus(),1);
+                }
+                else{
+                    dataBaseHelper.updateStatus(item.getStatus(),0);
+                }
+            }
+        });
 
     }
 
-    /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items in this adapter.
-     */
+    private boolean toBoolean(int status) {
+        return status!=0;
+    }
+    public Context getContext(){
+        return mainActivity;
+    }
+
+    public void setTask(List<ToDoModel> modelList){
+        this.modelList=modelList;
+        notifyDataSetChanged();
+    }
+    public void deleteTak(int position){
+        ToDoModel item=modelList.get(position);
+        dataBaseHelper.deleteTask(item.getId());
+        modelList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editTask(int position){
+        ToDoModel item=modelList.get(position);
+        Bundle bundle=new Bundle();
+        bundle.putInt("id",item.getId());
+        bundle.putString("Task",item.getTask());
+        bundle.putString("Discription",item.getDiscription());
+    }
+
+
     @Override
     public int getItemCount() {
-        return 0;
+
+        return modelList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        MaterialCheckBox idMaterialCheckBoxitemView;
+        TextView todo_list_task_descriptionitemView;
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            idMaterialCheckBoxitemView=itemView.findViewById(R.id.idMaterialCheckBoxitemView);
+            todo_list_task_descriptionitemView=itemView.findViewById(R.id.todo_list_task_description);
+
         }
     }
 }
